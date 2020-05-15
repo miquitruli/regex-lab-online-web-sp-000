59cd418a36f11495212e8aea1371cdfd73aa1e92
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-15 19:33:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,9 @@
 def starts_with_a_vowel?(word)
-  if word.(/^\w[aeiou]/)
+  if word.scan(/\A[aeiou]/)
     return true
-  end
+  else
+    nil
+  end  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
