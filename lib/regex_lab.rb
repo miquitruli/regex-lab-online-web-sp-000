a4a9597e20c6065f00e9116c6aeb1e653bfc938e
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-15 19:41:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/^[aeiou]/)
     return true
-  elsif word.scan(/\A[bcdfghjklmnpqrstvwxys]/)
+  elsif word.scan(/^[bcdfghjklmnpqrstvwxys]/)
     return false
   end
 end
