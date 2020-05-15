7b62bfcccc71590b6e20ed2b3e011750ebd12c9a
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-15 21:03:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/\w+^[aeiou]/)
     return true
-  elsif word.scam(/\w+^[[bcdfghjklmnpqrstvwxyz]/)
+  elsif word.scan(/\w+^[[bcdfghjklmnpqrstvwxyz]/)
     return false
   end
 
